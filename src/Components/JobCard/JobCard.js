67d1eef7c916e79c064fcfa60f26d7e1999d2812
Riyaz Ahmed <riyaz.mittu@gmail.com>
2024-05-05 03:34:33 +0530

    done with card's top section

--- src/Components/JobCard/JobCard.js
+++ src/Components/JobCard/JobCard.js
@@ -4,9 +4,23 @@ import './job-card.css';
 
 const JobCard = () => {
     return (
-        <main>
-             JOB CARD
-        </main>
+        
+        <div className="card__main">
+
+            <span></span>
+
+            <div className="card__top">
+                <div className="card__top__cont">
+                    <p>⏳ Posted 5 days ago</p>
+                </div>
+            </div>
+
+            <div className="card__middle"></div>
+
+            <div className="card__bottom"></div>
+
+        </div>
+        
     )
 }
 
