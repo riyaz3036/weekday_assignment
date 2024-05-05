2b9eb8c0f1dd482822c4c5d912399ed7a72435b5
Riyaz Ahmed <riyaz.mittu@gmail.com>
2024-05-05 20:31:26 +0530

    added dropdown menu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,108 @@ import './App.css';
 import JobCard from './Components/JobCard/JobCard';
 
 
+const roles_list = [
+  {
+    category: "ENGINEERING",
+    roles: [
+      "Backend", "Frontend", "Fullstack", "IOS", "Flutter", "React Native",
+      "Android", "Tech Lead", "Dev-Ops", "Data Engineer", "Data Science",
+      "Computer-Vision", "Nip", "Deep-Learning", "Test / Qa", "Web3", "Sre",
+      "Data-Infrastructure"
+    ]
+  },
+  {
+    category: "DESIGN",
+    roles: [
+      "Designer","Design Manager","Graphic Designer","Product Designer",
+    ]
+  },
+  {
+    category: "PRODUCT",
+    roles: [
+      "Product Manager"
+    ]
+  },
+  {
+    category: "OPERATIONS",
+    roles: [
+     "Operations Manager","Founder's Office/Chief Of Staff"
+    ]
+  },
+  {
+    category: "SALES",
+    roles: [
+     "Sales Development Representative","Account Executive","Account Manger"
+    ]
+  },
+  {
+    category: "MARKETING",
+    roles: [
+     "Digital Marketing Manager","Growth Hacker","Marketing","Product Marketing Manager"
+    ]
+  },
+  {
+    category: "OTHER ENGINEERING",
+    roles: [
+      "Hardware","Mechanical","Systems"
+    ]
+  },
+  {
+    category: "BUISINESS ANALYST",
+    roles: [
+      "Buisness Analyst"
+    ]
+  },
+  {
+    category: "DATA ANALYST",
+    roles: [
+      "Data Analyst"
+    ]
+  },
+  {
+    category: "PROJECT MANAGER",
+    roles: [
+      "Project Manager"
+    ]
+  },
+  {
+    category: "MANAGEMENT",
+    roles: [
+      "Management"
+    ]
+  },
+  {
+    category: "LEGAL",
+    roles: [
+      "Legal"
+    ]
+  },
+  {
+    category: "HR",
+    roles: [
+      "Hr"
+    ]
+  },
+  {
+    category: "FINANCE",
+    roles: [
+      "Finance"
+    ]
+  }
+];
+
+const techStack_list = [];
+
+const minBasePay_list = [0,10,20,30,40,50,60,70];
+
+const workMode_list = ["Remote","Hybrid","In-office"];
+
+const exp_list = [1,2,3,4,5,6,7,8,9,10];
+
+
 function App() {
+
+
   return (
     <div className="App">
 
@@ -29,6 +130,19 @@ function App() {
 
                 <div className="element__bottom">
 
+                    {
+                    roles_list.map((category, index) => (
+                        <div key={index} className="category">
+                            <p className="category__title">{category.category}</p>
+                            <div className="roles">
+                                {
+                                category.roles.map((role, roleIndex) => (
+                                    <div key={roleIndex} className="role">{role}</div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                    
                 </div>
             </div>
         </div>
